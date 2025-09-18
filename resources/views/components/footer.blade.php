cdede0c73da0f3385d42a27735ba60b6f95e554f
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-09-18 13:12:37 +0800

    0.6.5 fix bug ui

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -4,7 +4,7 @@
             &copy; 2025-{{ date('Y') }} SIDESA Anjir Muara. All Rights Reserved.
         </div>
         <div class="hidden md:inline-block">
-            Versi 0.6.4-alpha | Dibuat oleh Ahmad Syaifuddin - PKL UNISKA Banjarmasin
+            Versi 0.6.5-alpha | Dibuat oleh Ahmad Syaifuddin - PKL UNISKA Banjarmasin
         </div>
     </div>
 </footer>
